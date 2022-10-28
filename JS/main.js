101464c4a4464dec0417ded3440800b4f6486607
ser1372 <ser1372@ukr.net>
2022-10-28 20:02:41 +0300

    Done

--- JS/main.js
+++ JS/main.js
@@ -1,14 +1,22 @@
-var countInput = document.getElementById("count");
-var valueInput = document.getElementById("count").value;
-var plus = document.getElementById("plus");
-var form = document.getElementById("forma");
-var createBlock = document.getElementById("switch");
+const countInput = document.getElementById("count");
+const valueInput = document.getElementById("count").value;
+const plus = document.getElementById("plus");
+const form = document.getElementById("forma");
+const switcher = document.getElementById("switch");
+
+
+
+
+
+
 plus.onclick = function () {
   countInput.value = ++countInput.value;
 
   if (countInput.value == 100) {
     countInput.value = 1;
   }
+
+  console.log(countInput.value)
 };
 
 minus.onclick = function () {
@@ -17,21 +25,45 @@ minus.onclick = function () {
   if (countInput.value == 0) {
     countInput.value = ++countInput.value;
   }
+
+  else if(countInput.value == -1){
+    ++countInput.value;
+  }
 };
 
-
-
-function createMoon() {
+function chooseMoon(){
  
-
-  const moon = '<div class="moon"></div>';
-  const star = '<div class="star"</div>';
- 
-  document.body.innerHTML = moon;
-
-  document.body.style.background = "#000";
+  
 
   
+    document.body.innerHTML = moon;
+  
+}
+
+function createMoon() {
+  let checkbox = document.getElementById('checkboxs');
+  let result = ``;
+
+  if(checkbox.checked){
+    result = `<div class="moon"></div>`;
+  }
+
+  else{
+    result = `<div class="moonSecond"></div>`;
+  }
+  
+  document.body.style.background = "#000";
+  let stars = ``;
+  for (let i = 0; i < countInput.value; i++) {
+     result += "<div class=star></div>";
+
+  }
+  result + stars;
+
+
+  document.getElementById('body').innerHTML = result;
+  
+    console.log(document.body.querySelector('moon'))
  
 }
 
